75ec6deae7f6a6d68d176b5e928fedabb5e03ba6
iJustGhost <fryingchicken81@gmail.com>
2023-01-17 20:55:45 +0800

    small change

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import "./main.css";
 import Gate from "./Gate";
 import Content from "./Content";
@@ -8,13 +8,13 @@ import { ProtectedRoute } from "./ProtectedRoute";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Gate />} />
         <Route element={<ProtectedRoute />}>
           <Route path="/content" element={<Content />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
